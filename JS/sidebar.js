6fbaf578b4f52ae4c0e5ac4a4a9549e7acd0d223
Hemil <hemil.p19@gmail.com>
2025-05-15 12:44:52 +0100

    Sidebar toggle om desktop mode

--- JS/sidebar.js
+++ JS/sidebar.js
@@ -1,6 +1,11 @@
 const menuBtn = document.getElementById('menuBtn');
+const menu_desktopBtn = document.getElementById('menuBtn-desktop');
 const sidebar = document.querySelector('.sidebar');
 
 menuBtn.addEventListener('click', () => {
     sidebar.classList.toggle('active');
+});
+
+menu_desktopBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('small');
 });
